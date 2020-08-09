ae35a4a70bda936c00d0317bf8caf39f690bff83
김가은 <rkdms7220@naver.com>
2020-08-10 02:02:56 +0900

    face detection을 실행하는 코드

---
+++ face_detection.py
@@ -0,0 +1,67 @@
+import requests
+import cv2
+import numpy as np
+import os
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+id = 0
+
+#url="https://notify-api.line.me/api/notify"
+#TOKEN="k0bzAYAUtUsHp86IKH7ONcWLhkwODQ7tTWggEqgmlOC"
+
+names = ['None', 'gaeun', 'ljy', 'chs', 'ksw']
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video widht
+cam.set(4, 480) # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+while True:#사진들어오는 동안 실행
+    ret, img =cam.read()
+    img = cv2.flip(img, -1) # Flip vertically
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])#얼굴인식해서 정확도 알려주는 줄
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (confidence < 100):
+            print(confidence)
+            id = names[id]
+            cv2.imwrite("image/User."+id+".jpg",gray[y:y+h,x:x+w])#screen capture
+            confidence = "  {0}%".format(round(100 - confidence))
+            os.system('python line.py')
+            
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1) 
+
+    cv2.imshow('camera',img)#화면 띄우는 코드
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
