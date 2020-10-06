e7f7840f16cdb5844f822dd0c536ff3361f63b7d
김민지 <jee00609@naver.com>
2020-10-06 22:35:44 +0900

    pygame 패키지로 교체

--- QuickDraw/imagePredict.py
+++ QuickDraw/imagePredict.py
@@ -1,4 +1,4 @@
-## 목표 clear 기능 돌아가게 만들기
+## pygame 으로 실행할 시 mp3 1개 파일에 대한 재생이 무한히 반복되는 문제 발생
 
 import cv2
 from PIL import ImageTk, Image, ImageDraw
@@ -10,7 +10,8 @@ from keras.models import load_model
 import numpy as np
 from collections import deque
 import os
-from playsound import playsound
+# from playsound import playsound 교체
+import pygame, mutagen.mp3
 
 from PIL import ImageTk,ImageGrab,Image
 
@@ -20,10 +21,10 @@ center = height//2
 white = (255, 255, 255)
 green = (0,128,0)
 
+global flag
+flag= 0
+
 def clear():
-    ##Clear 에 대한 생각을 변경
-    ## 현재 내가 사용하는 코드에선 사용자에게 보이는 line 과 컴퓨터가 인식하는 line 두가지가 존재
-    ## 컴퓨터가 인식하는 line 을 지우는 것이 아닌 엄청 두꺼운 흰색 선을 다시 그리게 함으로써 캔버스를 깨끗이 한 것 같은 효과 줌
     cv.delete ("all")
     filename = "saveImage\\image.png"
     draw.line((0, 0, 400, 0),fill="white",width=800)
@@ -55,10 +56,13 @@ def predict():
             cnt = sorted(blackboard_cnts, key=cv2.contourArea, reverse=True)[0]
             print(cv2.contourArea(cnt))
             if cv2.contourArea(cnt) > 2000:
+                #list 에서 빼옴으로써 if 문 나열을 하지 않게 만들것!
+                predict_data = {0:"cloud",1:"lightning",2:"moon",3:"rain",4:"rainbow",5:"snowflake",6:"star",7:"sun",8:"tornado"}
+                
                 x, y, w, h = cv2.boundingRect(cnt)
                 digit = blackboard_gray[y:y + h, x:x + w]
                 pred_probab, pred_class = keras_predict(model, digit)
-                #여기서 pred_class 는 각이미지의 번호를 나타내는듯
+
                 print("hey!",pred_probab," and ", pred_class)
                 result_path1 = 'qd_emo\\'
                 result_path2 = '.png'
@@ -68,41 +72,11 @@ def predict():
                 resultImage = ImageTk.PhotoImage(file=result)
                 label.configure(image=resultImage)
                 label.image = resultImage # keep a reference!
-#                 print("DONE!")
-                #2020-08-18 그림 인식 시 어떤 그림인지 텍스트로 표현
-                if pred_class==0:
-                    text.set("This is apple!")
-                elif pred_class==1:
-                    text.set("This is Book!")
-                elif pred_class==2:
-                    text.set("This is Bowtie!")
-                elif pred_class==3:
-                    text.set("This is Broom!")
-                elif pred_class==4:
-                    text.set("This is Candle!")
-                elif pred_class==5:
-                    text.set("This is Door!")
-                elif pred_class==6:
-                    text.set("This is Envelope!")
-                elif pred_class==7:
-                    text.set("This is Fish!")
-                elif pred_class==8:
-                    text.set("This is Guitar!")
-                elif pred_class==9:
-                    text.set("This is Ice Cream!")
-                elif pred_class==10:
-                    text.set("This is Lightning!")
-                elif pred_class==11:
-                    text.set("This is Moon!")
-                elif pred_class==12:
-                    text.set("This is Mountain!")
-                elif pred_class==13:
-                    text.set("This is Star!")
-                elif pred_class==14:
-                    text.set("This is Tent!")
-                elif pred_class==15:
-                    text.set("This is Wristwatch!")
                 
+                robot_answer = predict_data[pred_class]
+                text.set(robot_answer)
+                
+
     elif os.path.isdir(file):
         print("Yes. it is a directory")
     elif os.path.exists(file):
@@ -114,7 +88,6 @@ def voice():
     model = load_model('QuickDraw.h5')
     file = 'saveImage\\image.png'
     emojis = get_QD_emojis()
-
 
     if os.path.isfile(file):
         print("Yes. it is a file")
@@ -136,8 +109,25 @@ def voice():
                 x, y, w, h = cv2.boundingRect(cnt)
                 digit = blackboard_gray[y:y + h, x:x + w]
                 pred_probab, pred_class = keras_predict(model, digit)
-                if pred_class==0:
-                    playsound('saveAudio\\sampleAudio.mp3')
+                print("heyhey!",pred_probab," and ", pred_class)
+                
+                global flag
+                print(flag)
+                voice_name = pred_class
+
+                if flag ==0:
+                    #Female Voice
+                    sound_dir = "saveAudio\\female\\"+str(voice_name)+".mp3"
+                    print(sound_dir)
+                    ## https://kkamikoon.tistory.com/135 참고
+                    ## 그는 신이야!
+                    playmusic(sound_dir)
+                    stopmusic()
+                elif flag ==1:
+                    #Male Voice
+                    sound_dir = "saveAudio\\male\\"+str(voice_name)+".mp3"
+                    playmusic(sound_dir)
+                    stopmusic()
                 
     elif os.path.isdir(file):
         print("Yes. it is a directory")
@@ -145,15 +135,46 @@ def voice():
         print("Something exist")
     else :
         print("Nothing")
+        
+def playmusic(soundfile):
+    """Stream music with mixer.music module in blocking manner.
+       This will stream the sound from disk while playing.
+    """
+    pygame.init()
+
+    bitsize = -16   # signed 16 bit. support 8,-8,16,-16
+    channels = 1    # 1 is mono, 2 is stereo
+    buffer = 2048   # number of samples (experiment to get right sound)
+    mp3 = mutagen.mp3.MP3(soundfile)
+    frequency=mp3.info.sample_rate
+    
+    pygame.mixer.init()
+    clock= pygame.time.Clock()
+    pygame.mixer.music.load(soundfile)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+#         print("Playing... - func => playingmusic")
+        clock.tick(1000)
+
+def stopmusic():
+    """stop currently playing music"""
+    pygame.mixer.music.stop()
+
+        
+def flag():
+    global flag
+    
+    if flag==0:
+        flag=1
+    else:
+        flag=0
 
 def paint(event):
     # python_green = "#476042"
     x1, y1 = event.x, event.y
     if cv.old_coords:
         x2, y2 = cv.old_coords
-        ## 사용자에게 시각적으로 보여주기 위한 line
         cv.create_line(x1, y1, x2, y2, fill="black",width=5)
-        ## 실제로 컴퓨터가 인식하는 line
         draw.line([x1, y1, x2, y2],fill="black",width=5)
     cv.old_coords = x1, y1
     
@@ -167,6 +188,7 @@ def keras_predict(model, image):
     processed = keras_process_image(image)
     print("processed: " + str(processed.shape))
     pred_probab = model.predict(processed)[0]
+    print("keras_predict pred_probab = ",pred_probab)
     pred_class = list(pred_probab).index(max(pred_probab))
     return max(pred_probab), pred_class
 
@@ -209,7 +231,7 @@ cv.bind("<ButtonRelease-1>", reset_coords)
 root.bind("<Escape>", lambda e: root.destroy())
 
 text= StringVar(root)
-text.set("그림을 그리면 알아맞출게요!\n")
+text.set("Draw a picture and I'll guess!\n")
 # text.configure(state="disabled")
 textlabel = Label(root, textvariable=text)
 textlabel.place(x=400, y=0, width=400, height=30)
@@ -227,6 +249,10 @@ button.place(x=300, y=400, width=100, height=20)
 
 button=Button(text="voice",command=voice)
 button.place(x=550, y=350, width=100, height=20)
+
+button=Button(text="Flag",command=flag)
+button.place(x=760, y=380, width=40, height=40)
+flag = 0
 
 
 root.mainloop()
